032b7ce4847866b21541d56507531343fc3e5d22
fadimata tillman <fadi.tillman@gmail.com>
2020-02-02 19:09:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,6 +27,10 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+                 shipping_manifest["muskets"]=2
+                 shipping_manifest ["gun powder"] =4
+               end
+              
   # add 2 muskets to the shipping_manifest hash below
 
 
